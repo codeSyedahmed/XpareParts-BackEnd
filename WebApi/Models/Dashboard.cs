419e1a22d43e8744bf129770a71ce3cf336eaaf3
syedahmed <code.syedahmed@gmail.com>
2024-01-21 23:58:35 +0500

    second commit

--- WebApi/Models/Dashboard.cs
+++ WebApi/Models/Dashboard.cs
@@ -1,11 +1,10 @@
 ﻿namespace WebApi.Models
 {
-    public class Shops
+    public class Dashboard
     {
         public int shops { get; set; }
         public int wholesellers { get; set; }
         public int revenue { get; set; }
         public int customers { get; set; }
-
     }
 }
